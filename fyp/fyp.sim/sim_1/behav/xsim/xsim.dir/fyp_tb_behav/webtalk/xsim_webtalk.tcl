936aa8a9b1c05447f5599195c372a9bffd130077
unknown <kennylam51@ymail.com>
2021-11-27 01:19:48 +0800

    Update Vivado caches

--- fyp/fyp.sim/sim_1/behav/xsim/xsim.dir/fyp_tb_behav/webtalk/xsim_webtalk.tcl
+++ fyp/fyp.sim/sim_1/behav/xsim/xsim.dir/fyp_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Data/FYP/fyp/fyp.sim/sim_1/behav/xsim/xsim.dir/fyp_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Nov 23 22:43:36 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat Nov 27 01:06:34 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "f71adbbf-4851-4757-96e8-cb5348a3a1a7" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "06a9eb38ad844bce8014224215b729c1" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "6" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "32" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i3-7100U CPU @ 2.40GHz" -context "user_environment"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "use
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "1001 us" -context "xsim\\usage"
-webtalk_add_data -client xsim -key iteration -value "4" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "1.00_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "13956_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3276215696 -regid "212288350_0_0_009" -xml C:/Data/FYP/fyp/fyp.sim/sim_1/behav/xsim/xsim.dir/fyp_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Data/FYP/fyp/fyp.sim/sim_1/behav/xsim/xsim.dir/fyp_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Data/FYP/fyp/fyp.sim/sim_1/behav/xsim/xsim.dir/fyp_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key iteration -value "5" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.16_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "5992_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1837497702 -regid "212288350_0_0_009" -xml C:/Data/FYP/fyp/fyp.sim/sim_1/behav/xsim/xsim.dir/fyp_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Data/FYP/fyp/fyp.sim/sim_1/behav/xsim/xsim.dir/fyp_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Data/FYP/fyp/fyp.sim/sim_1/behav/xsim/xsim.dir/fyp_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
