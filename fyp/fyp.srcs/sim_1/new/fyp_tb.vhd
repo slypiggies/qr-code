265a4f1c23b0a937f3dbc01597c473daa121559a
unknown <kennylam51@ymail.com>
2021-11-27 01:17:21 +0800

    Update testbenches

--- fyp/fyp.srcs/sim_1/new/fyp_tb.vhd
+++ fyp/fyp.srcs/sim_1/new/fyp_tb.vhd
@@ -16,13 +16,13 @@ architecture fyp_tb_a of fyp_tb is
 	
 	constant ADDR: std_logic_vector(7 downto 0) := X"42";
 	constant D: std_logic_vector(15 downto 0) := B"01010101_00110011";
-	constant EN: std_logic := '1';
 	
 	constant H: natural := 4;
 	constant V: natural := 3;
 	constant ADDR_LENGTH: natural := natural(floor(log2(real(H * V)))) + 1;
 	constant PIXEL_LENGTH: natural := 4;
 	constant PIXEL_R: unsigned(PIXEL_LENGTH - 1 downto 0) := X"F";
+	constant PROCESSED_PIXEL_LENGTH: natural := PIXEL_LENGTH * 3;
 	
 	constant PERIOD100: time := 10 ns;
 	signal clk100: std_logic := '1';
@@ -30,7 +30,7 @@ architecture fyp_tb_a of fyp_tb is
 	
 	signal scl, sda, tx_ed: std_logic;
 	signal addr_r, addr_w: unsigned(ADDR_LENGTH - 1 downto 0);
-	signal pixel_w: unsigned(PIXEL_LENGTH - 1 downto 0);
+	signal pixel_w: unsigned(PROCESSED_PIXEL_LENGTH - 1 downto 0);
 	signal we: std_logic;
 begin
 	process begin
@@ -52,7 +52,7 @@ begin
 			scl => scl,
 			sda => sda,
 			tx_ed => tx_ed,
-			en => EN
+			en => '1'
 		);
 	end generate;
 	
@@ -61,7 +61,8 @@ begin
 			H => H,
 			V => V,
 			ADDR_LENGTH => ADDR_LENGTH,
-			PIXEL_LENGTH => PIXEL_LENGTH
+			PIXEL_LENGTH => PIXEL_LENGTH,
+			PROCESSED_PIXEL_LENGTH => PROCESSED_PIXEL_LENGTH
 		) port map (
 			reset => reset,
 			clk100 => clk100,
--- fyp/fyp.srcs/sim_1/new/kernel3_tb.vhd
+++ fyp/fyp.srcs/sim_1/new/kernel3_tb.vhd
@@ -8,21 +8,21 @@ entity kernel3_tb is
 	generic (
 		H, V: natural;
 		ADDR_LENGTH: natural;
-		PIXEL_LENGTH: natural
+		PIXEL_LENGTH: natural;
+		PROCESSED_PIXEL_LENGTH: natural
 	);
 	port (
 		reset, clk100: in std_logic;
 		addr_r, addr_w: out unsigned(ADDR_LENGTH - 1 downto 0);
 		pixel_r: in unsigned(PIXEL_LENGTH - 1 downto 0);
-		pixel_w: out unsigned(PIXEL_LENGTH - 1 downto 0);
+		pixel_w: out unsigned(PROCESSED_PIXEL_LENGTH - 1 downto 0);
 		we: out std_logic
 	);
 end entity;
 
 architecture kernel3_tb_a of kernel3_tb is
-	constant KERNEL: integer_vector := (1, 0, -1, 2, 0, -2, 1, 0, -1);
-	constant PROCESSED_PIXEL_LENGTH: natural := PIXEL_LENGTH * 3;
-	constant NORM_SHIFT: natural := 2;
+	constant KERNEL: integer_vector(0 to 8) := (-1, -2, -1, 0, 0, 0, 0, 0, 0);
+	constant THRESHOLD: natural := 16;
 	signal state: unsigned(3 downto 0);
 begin
 	kernel3_controller_i: entity kernel3_controller generic map (
@@ -42,7 +42,7 @@ begin
 		PIXEL_LENGTH => PIXEL_LENGTH,
 		KERNEL => KERNEL,
 		PROCESSED_PIXEL_LENGTH => PROCESSED_PIXEL_LENGTH,
-		NORM_SHIFT => NORM_SHIFT
+		THRESHOLD => THRESHOLD
 	) port map (
 		CLK100 => clk100,
 		state => state,
